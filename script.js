b00b9dd77f33a34fb1db4f29a21116444b6e1ee7
AuraSA2439 <aurasukma.auliaa@gmail.com>
2024-11-22 20:31:08 +0700

    Tambah table

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function verify_login() {
     location.assign("index.html");
 }
 
-// function verify_login() {
+function report_response() {
+    document.getElementById("popup-response-click").style.display = "block";
+}
+
+function closePopup() {
+    document.querySelector(".popup-wrap").style.display = "none";
+  }
+
+// function report_response() {
+//     document.querySelector(".popout-response").style.display = "block";
+// }
+
+// function idke() {
 //     document.getElementById("dropdown_account").style.display = "block";
 // }
