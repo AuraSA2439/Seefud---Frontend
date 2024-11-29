ff1a21d724c04cccb003c26c7426d9dc76ba8f7d
AuraSA2439 <aurasukma.auliaa@gmail.com>
2024-11-29 17:38:30 +0700

    Page vendor data, report data dll

--- script.js
+++ script.js
@@ -29,10 +29,19 @@ function report_response() {
     document.getElementById("popup-response-click").style.display = "block";
 }
 
+function report_counts_popup() {
+    document.getElementById("popup-report-click").style.display = "block";
+}
+
 function closePopup() {
-    document.querySelector(".popup-wrap").style.display = "none";
-    document.querySelector(".popup-wrap-copy1").style.display = "none";
-    document.querySelector(".popup-wrap-copy2").style.display = "none";
+    const popups = document.querySelectorAll(".popup-wrap, .popup-wrap-copy1, .popup-wrap-copy2, .popup-wrap-copy3, .popup-wrap-copy4");
+
+    for (let i = popups.length - 1; i >= 0; i--) {
+        if (popups[i].style.display !== "none") {
+            popups[i].style.display = "none";
+            break;
+        }
+    }
 }
 
 function edit_popup() {
@@ -56,4 +65,41 @@ function delete_data() {
 
 // function idke() {
 //     document.getElementById("dropdown_account").style.display = "block";
+// }
+
+// Perlu tydak drag kira2? agak mager nambahin hehe :)
+// dragElement(document.querySelector(".popup"));
+
+// function dragElement(elmnt) {
+//   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+//   if (document.getElementById(elmnt.id + "button")) {
+//     document.getElementById(elmnt.id + "button").onmousedown = dragMouseDown;
+//   } else {
+//     elmnt.onmousedown = dragMouseDown;
+//   }
+
+//   function dragMouseDown(e) {
+//     e = e || window.event;
+//     e.preventDefault();
+//     pos3 = e.clientX;
+//     pos4 = e.clientY;
+//     document.onmouseup = closeDragElement;
+//     document.onmousemove = elementDrag;
+//   }
+
+//   function elementDrag(e) {
+//     e = e || window.event;
+//     e.preventDefault();
+//     pos1 = pos3 - e.clientX;
+//     pos2 = pos4 - e.clientY;
+//     pos3 = e.clientX;
+//     pos4 = e.clientY;
+//     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+//     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+//   }
+
+//   function closeDragElement() {
+//     document.onmouseup = null;
+//     document.onmousemove = null;
+//   }
 // }
